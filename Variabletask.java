736e618d45b1260c03f9326a08b0f04bfa20ac72
LakshmanLRM <luxjr1175@gmail.com>
2025-04-03 15:44:58 +0530

    Day-2

---
+++ Variabletask.java
@@ -0,0 +1,18 @@
+public class Variabletask {
+       public static void main(String[]args) {
+    	   byte a=17;
+    	   short b=32000;
+    	   int c=2000000;
+    	   long d=76447838;
+    	   double e=34.474584;
+    	   float f=43.65f;
+    	   boolean g=true;
+    	   System.out.print(a);
+    	   System.out.print(b);
+    	   System.out.print(c);
+    	   System.out.print(d);
+    	   System.out.print(e);
+    	   System.out.print(f);
+    	   System.out.print(g);
+       }
+}
