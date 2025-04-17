fe348cddbf664d0ac6c7e7dcce4f070844cb7629
LakshmanLRM <luxjr1175@gmail.com>
2025-04-17 13:14:38 +0530

    day8

---
+++ student.java
@@ -0,0 +1,23 @@
+package day8;
+
+public class student {
+  String name;
+  long number;
+	public static void main(String[] args) {
+		student  s1 = new student();
+		s1.name = "lakshman";
+		s1.number = 9381033305l;
+		System.out.println(s1.name+s1.number);
+		student  s2 = new student();	
+		s2.name = "prassana";
+		s2.number = 9381033455l;
+		System.out.println(s2.name+s2.number);
+		student  s3 = new student();
+		s3.name = "perumal";
+		s3.number = 9382223305l;
+		System.out.println(s3.name+s3.number);
+		
+
+	}
+
+}
