431839e893f08e3c48fc7942d5ba288f07b352da
LakshmanLRM <luxjr1175@gmail.com>
2025-05-07 21:00:46 +0530

    Day-3

---
+++ Operators.java
@@ -0,0 +1,13 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		  Scanner sc =new Scanner(System.in);
+		  String name= sc.next();
+		  System.out.println(name);
+	}
+
+}
