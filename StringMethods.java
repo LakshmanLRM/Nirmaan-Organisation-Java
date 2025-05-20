b2ee61b995b84b6b1f7df131d3d91bcc89cc77a2
LakshmanLRM <luxjr1175@gmail.com>
2025-05-20 10:47:44 +0530

    Day-14

---
+++ StringMethods.java
@@ -0,0 +1,16 @@
+package day14;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+         String a = "name";
+         String b = "lux";
+         System.out.println(a.toLowerCase()+""+b.toLowerCase());
+         System.out.println(b.toUpperCase()+""+a.toLowerCase());
+        System.out.println(b.equalsIgnoreCase(b));
+        System.out.println(b.contains("x"));
+         
+       
+	}
+
+}
